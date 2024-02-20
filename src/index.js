8bb9fc4706452960c5a875b61378be6a9e069e8f
Abhijit <abhijeetpurvey234@gmail.com>
2024-02-20 11:56:14 +0530

    Added notification ticket model

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ const bodyParser= require('body-parser');
 
 const {PORT}=require('./config/serverConfig');
 
-const {sendBasicEmail}= require('./services/email-service');
-
+//const {sendBasicEmail}= require('./services/email-service');
+const cron = require('node-cron');
 const setupAndStartServer = () => {
     const app=express();
     app.use(bodyParser.json());
@@ -12,12 +12,16 @@ const setupAndStartServer = () => {
 
     app.listen(PORT,async()=> {
         console.log(`Server started at port ${PORT}`);
-        sendBasicEmail(
-            'abhijeetpurvey234@gmail.com',
-            'anubhavshivahre@gmail.com',
-            'This is a testing email',
-            'Hey, how are you, I hope you like the support'
-        );
+        // sendBasicEmail(
+        //     'abhijeetpurvey234@gmail.com',
+        //     'anubhavshivahre@gmail.com',
+        //     'This is a testing email',
+        //     'Hey, how are you, I hope you like the support'
+        // );
+
+        cron.schedule('*/2 * * * *',()=> {
+            console.log('Running a task every two minutes');
+        });
     });
 }
 
